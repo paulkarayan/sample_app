a2755e24f65bd1841a901ab98af5e31c49e5b47b
paulkarayan <paulkarayan@gmail.com>
2013-11-01 22:57:57 -0700

    finish routes and layout

---
+++ playhash.rb
@@ -0,0 +1,6 @@
+person1 = {first: "paul", last: "karayan"}
+person2 = {first: "laura", last: "serwer"}
+person3 = {first: "milo", last: "karayan"}
+
+params = {father: person1, mother: person2, child: person3  }
+
