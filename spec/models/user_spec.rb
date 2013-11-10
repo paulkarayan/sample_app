81bd88cf53ef659dbd8a821c0b23145b19f5a7d8
paulkarayan <paulkarayan@gmail.com>
2013-11-10 15:50:27 -0800

    addtl tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,7 +49,7 @@ describe User do
     describe "when email format is invalid" do
 	    it "should be invalid" do
 	      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-	                     foo@bar_baz.com foo@bar+baz.com]
+	                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 	      addresses.each do |invalid_address|
 	        @user.email = invalid_address
 	        expect(@user).not_to be_valid
@@ -67,6 +67,17 @@ describe User do
 		end
 	end
 	end
+
+  describe "when email is of mixed case" do
+    let(:mixed_case_email)  { "MiXed@CasEmail.com" }
+    
+    it "should be saved as all lower case" do
+    @user.email = mixed_case_email
+    @user.save
+    expect(@user.reload.email).to eq mixed_case_email.downcase
+  end
+  end
+
 
 
 	describe "when email address already taken" do
